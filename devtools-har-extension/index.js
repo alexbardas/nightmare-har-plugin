2f60f8125f2f467c0af28567513cd14beb00c428
Wandenberg Peixoto <wpe@tradeshift.com>
2017-06-25 11:45:01 +0200

    Add support to collect HAR data from all visited pages
    When the flow goes through multiple pages it keeps data for all of them, not only the last visited page.
    This allow retrieve data for more complex use cases like, enter on the site, do a search and check the result.
    It also add a `resetHAR` function to allow clear the collected data anytime.

--- devtools-har-extension/index.js
+++ devtools-har-extension/index.js
@@ -1,4 +1,14 @@
 const REQUEST_FINISHED_URL = 'chrome://version/?get-har'
+const REQUEST_RESET_URL = 'chrome://version/?reset-har'
+
+let har = {
+  'version': '1.2',
+  'creator': { 'name': '', 'version': '' },
+  'entries': [],
+  'pages': []
+}
+
+let pageRef = 0
 
 let send = (channel, msg) => {
   window.chrome.devtools.inspectedWindow.eval(
@@ -6,15 +16,44 @@ let send = (channel, msg) => {
   )
 }
 
-let sendHAR = () => {
-  window.chrome.devtools.network.getHAR((har) => {
-    har.entries = har.entries.filter((entry) => entry.request.url !== REQUEST_FINISHED_URL)
-    send('har-data', har)
-  })
-}
+window.chrome.devtools.network.getHAR((info) => {
+  har.creator.name = info.creator.name
+  har.creator.version = info.creator.version
+})
 
 window.chrome.devtools.network.onRequestFinished.addListener((request) => {
-  if (request.request.url === REQUEST_FINISHED_URL) {
-    sendHAR()
+  switch (request.request.url) {
+    case REQUEST_FINISHED_URL:
+      send('har-data', har)
+      break
+    case REQUEST_RESET_URL:
+      har.entries = []
+      har.pages = []
+      pageRef = 1
+      send('har-reset')
+      break
+    default:
+      har.entries.push(Object.assign({}, request, { 'pageref': 'page_' + pageRef }))
+      break
+  }
+})
+
+window.chrome.devtools.network.onNavigated.addListener((request) => {
+  pageRef += 1
+})
+
+// Create a connection to the background page
+let backgroundPageConnection = window.chrome.runtime.connect({
+  name: 'panel'
+})
+
+backgroundPageConnection.postMessage({
+  name: 'init',
+  tabId: window.chrome.devtools.inspectedWindow.tabId
+})
+
+backgroundPageConnection.onMessage.addListener(function (message) {
+  if (message.type === 'page') {
+    har.pages.push(Object.assign({ 'id': 'page_' + pageRef }, message.data))
   }
 })
